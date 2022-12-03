f1d5c0c52da9f821c17e7392c228fcf55df2b32f
korolyovranext <korolyovranext@localhost.localdomain>
2022-12-03 18:35:31 +0300

    all commit

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '0eeff27f9c89ab975e01c1eb5aeef5148b1810e9b690a77bedd94a261b3d9b98'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://flask:Passw0rd!@postgres:5432/flask'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     bcrypt.init_app(app)
@@ -46,4 +46,4 @@ def create_database(app):
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
